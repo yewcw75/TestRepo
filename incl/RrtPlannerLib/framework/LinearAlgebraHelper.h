49037f0f8c862692b77a1ae8ae1908b990578d4d
LG-YEW\yewcw <yewcw75@gmail.com>
2023-06-29 00:07:37 +0800

    update LinearAlgebraHelper

--- incl/RrtPlannerLib/framework/LinearAlgebraHelper.h
+++ incl/RrtPlannerLib/framework/LinearAlgebraHelper.h
@@ -6,8 +6,12 @@
 #define RRTPLANNER_LIB_LINEARALGEBRAHELPER_H
 
 #include <RrtPlannerLib/RrtPlannerLibGlobal.h>
+#include <RrtPlannerLib/framework/FrameworkDefines.h>
 #include <boost/numeric/ublas/vector.hpp>
 #include <boost/numeric/ublas/matrix.hpp>
+
+namespace bg = boost::geometry;
+namespace bnu = boost::numeric::ublas;
 
 RRTPLANNER_FRAMEWORK_BEGIN_NAMESPACE
 
@@ -17,10 +21,16 @@ public:
     LinearAlgebraHelper();
     ~LinearAlgebraHelper();
 
-    static bool solve(const boost::numeric::ublas::matrix<float>& M, //invertible square matrix, [NxN]
-                      const boost::numeric::ublas::vector<float>& y, //[Nx1]
+    static bnu::vector<float> to_bnu_vector(const Bgm_Point& vec);
+    static Bgm_Point to_bgm_point(const bnu::vector<float>& vec);
+    static bnu::matrix<float> concatenate_col_vectors(const bnu::vector<float>& v1,
+                                                      const bnu::vector<float>& v2
+                                                      ); //treat v1 and v2 as col vectors, and concatenate column-wise. i.e. [v1, v2]
+
+    static bool solve(const bnu::matrix<float>& M, //invertible square matrix, [NxN]
+                      const bnu::vector<float>& y, //[Nx1]
                       float tol_small, //for checking if ok to 1/det(M)
-                      boost::numeric::ublas::vector<float>& x //[Nx1]
+                      bnu::vector<float>& x //[Nx1]
                       );
 
 };
