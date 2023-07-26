93252f4628c3dabc7677d2c2834886d0c817e9e9
LG-YEW\yewcw <yewcw75@gmail.com>
2023-07-26 17:24:39 +0800

    EllMap >> shift implementation of buildEllMap to mp_pimpl.

--- src/framework/EllMap.cpp
+++ src/framework/EllMap.cpp
@@ -24,10 +24,50 @@ public:
           mp_planNominal(rhs.mp_planNominal)
     {}
 
+    bool buildEllMap(double crossTrackHorizon,
+                             QString* results_desc);
+
 public:
     QList<QSharedPointer<Plan>> m_planSharedPtrList;
     QSharedPointer<Plan> mp_planNominal;
 };
+
+//----------
+bool EllMapData::buildEllMap(double crossTrackHorizon,
+                         QString* results_desc)
+{
+    bool ret = true;
+    QString results_desc_local;
+
+    if(mp_planNominal) { //pointer is not null
+        QList<double> sideList{-1.0, 1.0}; //use in for loop to find ellmaps for port side first, then stbd side.
+        for(const auto& side: sideList){
+            ret = PlanHelper::buildSingleSideEllMap(mp_planNominal,
+                                                    side,
+                                                    crossTrackHorizon,
+                                                    m_planSharedPtrList,
+                                                    &results_desc_local);
+            if(!ret){
+                break; //break for loop and returns if error
+            }
+        } //for(const auto& side: sideList)
+    }
+    else{ //mp_pimpl->mp_planNominal is null
+        ret = false;
+        results_desc_local = QString("[EllMap::buildEllMap] Build EllMap fails. Nominal plan is not set yet.");
+    }
+
+    //give the plans an id
+    for(int i = 0; i < m_planSharedPtrList.size(); ++i){
+        m_planSharedPtrList.at(i)->setId(i);
+    }
+
+    //set output result description if input pointer is not null
+    if(results_desc){
+        *results_desc = results_desc_local;
+    }
+    return(ret);
+}
 
 //#################
 //----------
@@ -70,37 +110,7 @@ void EllMap::setNominalPlan(const Plan& plan)
 bool EllMap::buildEllMap(double crossTrackHorizon,
                          QString* results_desc)
 {
-    bool ret = true;
-    QString results_desc_local;
-
-    if(mp_pimpl->mp_planNominal) { //pointer is not null
-        QList<double> sideList{-1.0, 1.0}; //use in for loop to find ellmaps for port side first, then stbd side.
-        for(const auto& side: sideList){
-            ret = PlanHelper::buildSingleSideEllMap(mp_pimpl->mp_planNominal,
-                                                    side,
-                                                    crossTrackHorizon,
-                                                    mp_pimpl->m_planSharedPtrList,
-                                                    &results_desc_local);
-            if(!ret){
-                break; //break for loop and returns if error
-            }
-        } //for(const auto& side: sideList)
-    }
-    else{ //mp_pimpl->mp_planNominal is null
-        ret = false;
-        results_desc_local = QString("[EllMap::buildEllMap] Build EllMap fails. Nominal plan is not set yet.");
-    }
-
-    //give the plans an id
-    for(int i = 0; i < mp_pimpl->m_planSharedPtrList.size(); ++i){
-        mp_pimpl->m_planSharedPtrList.at(i)->setId(i);
-    }
-
-    //set output result description if input pointer is not null
-    if(results_desc){
-        *results_desc = results_desc_local;
-    }
-    return(ret);
+    return(mp_pimpl->buildEllMap(crossTrackHorizon, results_desc));
 }
 
 //----------
